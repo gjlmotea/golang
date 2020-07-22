211d76e7b9f54a5e9e8968d7598ec5861b27dd1a
gjlmotea <gjlmotea@gmail.com>
2020-07-22 16:07:12 +0800

    create/alter table method

---
+++ practice_sql_03/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+var db *sql.DB
+
+func main(){
+	connectDb()
+
+	//createDb()
+
+	//createTable()
+
+	//alterTable()
+
+	//insert()
+
+	query()
+}
+
+func connectDb(){
+	Dbconn, err := sql.Open("mysql", "root:8888@tcp(127.0.0.1:3306)/?parseTime=true")
+	if err != nil{
+		log.Fatal(err)
+	}
+	//defer Dbconn.Close()
+
+	db = Dbconn
+	db.SetMaxOpenConns(1)
+	db.SetMaxIdleConns(1)
+	err = db.Ping()
+	if err != nil{
+		log.Fatalln(err)
+	}
+
+}
+
+func createDb(){
+	_, err := db.Exec("CREATE DATABASE `test`;")
+	if err != nil{
+		log.Fatalln(err)
+	}
+}
+
+func createTable() {
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS test.hello(world varchar(50))")
+	if err != nil{
+		log.Fatalln(err)
+	}
+}
+
+func alterTable()  {
+	//_, err := db.Exec("ALTER TABLE test.hello ADD COLUMN id int(10)")
+	_, err := db.Exec("ALTER TABLE test.hello ADD COLUMN id int(10) PRIMARY KEY AUTO_INCREMENT")
+	if err != nil{
+		log.Fatalln(err)
+	}
+}
+
+func insert() {
+	rs, err := db.Exec("INSERT INTO test.hello(world) VALUES ('wqqqq')")
+	if err != nil{
+		log.Fatalln(err)
+	}
+	rowCount, err := rs.RowsAffected()
+	rowId , err := rs.LastInsertId()
+	if err != nil{
+		log.Fatalln(err)
+	}
+	log.Printf("inserted %d rows where id = %d", rowCount, rowId)
+}
+
+func query(){
+	rows, err := db.Query("SELECT world FROM test.hello")
+	if err != nil{
+		log.Fatalln(err)
+	}
+
+	for rows.Next(){
+		var s string
+		err = rows.Scan(&s)
+		if err !=nil{
+			log.Fatalln(err)
+		}
+		log.Printf("found row containing %q", s)
+	}
+	rows.Close()
+}
